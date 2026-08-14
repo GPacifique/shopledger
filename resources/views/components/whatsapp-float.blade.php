86127d0ea3cc21216cd2416554577bc867550321
BeanLeaf <137269259+GPacifique@users.noreply.github.com>
2026-08-14 11:10:19 +0200

    wtsapp button

--- resources/views/components/whatsapp-float.blade.php
+++ resources/views/components/whatsapp-float.blade.php
@@ -1,4 +1,4 @@
-<a href="https://wa.me/2507XXXXXXXX?text=Hello%20I%20need%20support"
+<a href="https://wa.me/250786163963?text=Hello%20I%20need%20support"
    class="whatsapp-float"
    target="_blank"
    rel="noopener noreferrer">
